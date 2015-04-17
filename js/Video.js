b8f4ddae1336cea7cf1840b5aa3c5b3a36008ed8
jairzijp <jairzijp123@gmail.com>
2015-04-17 21:09:31 +0200

    duration van video en nog wat kleine aanpassingen

--- js/Video.js
+++ js/Video.js
@@ -1,9 +1,11 @@
-var Video = function(video, playButton, timeLine, muteButton){
-	this.video = document.getElementById("video1");
+var Video = function(){
+	this.video = document.getElementsByTagName('video')[1];
 	this.playButton = document.getElementById("playPauseToggle");
 	this.timeLine = document.getElementById("timeLine");
 	this.muteButton = document.getElementById('mute');
 	this.fullScreen = document.getElementById('fullscreen');
+	this.currentTime = document.getElementById('currentTime');
+	this.durationTime = document.getElementById('durationTime');
 }
 
 Video.prototype.playPause = function()
@@ -19,25 +21,38 @@ Video.prototype.playPause = function()
 	
 }
 
-Video.prototype.vidSeek = function(seekto)
+Video.prototype.dragTime = function(time)
 {
-	this.seekto = this.video.duration * (this.timeLine.value / 100);
-	this.video.currentTime = this.seekto;
+	time = this.video.duration * (this.timeLine.value / 100);
+	this.video.currentTime = time;
 }
 
-Video.prototype.timeUpdate = function()
+Video.prototype.timeUpdate = function(currentMinutes, currentSeconds, durationMinutes, durationSeconds)
 {
 	this.timeLine.value = this.video.currentTime * (100 / this.video.duration);
+	currentMinutes = Math.floor(this.video.currentTime / 60);
+	currentSeconds = Math.floor(this.video.currentTime - currentMinutes * 60);
+	durationMinutes = Math.floor(this.video.duration / 60);
+	durationSeconds = Math.round(this.video.duration - durationMinutes * 60);
+	if(currentSeconds < 10){
+		currentSeconds = "0"+currentSeconds;
+	}
+	if(durationSeconds < 10){
+		durationSeconds = "0"+durationSeconds;
+	}
+	this.currentTime.innerHTML = currentMinutes + ":" + currentSeconds;
+	this.durationTime.innerHTML = durationMinutes + ":" + durationSeconds;
+	
 }
 
 Video.prototype.mute = function () 
 {
 	if(this.video.muted){
 		this.video.muted = false;
-		this.muteButton.innerHTML = "Mute";
+		this.muteButton.innerHTML = "&#128266;";
 	} else {
 		this.video.muted = true;
-		this.muteButton.innerHTML = "Unmute";
+		this.muteButton.innerHTML = "Unmute	";
 	}
 }
 
--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ var video1 = new Video();
 
 video1.playButton.addEventListener('click',function(){ video1.playPause()  });
 video1.video.addEventListener("timeupdate",function(){ video1.timeUpdate() });
-video1.timeLine.addEventListener("change", function(){ video1.vidSeek()    });
+video1.timeLine.addEventListener("change", function(){ video1.dragTime()   });
 video1.muteButton.addEventListener("click",function(){ video1.mute()	   });
 video1.fullScreen.addEventListener("click",function(){ video1.fullScreenMode()   });
